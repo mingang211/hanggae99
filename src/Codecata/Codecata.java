c2fb6d0698f038d1808a8936ade98996587dbaf9
mingang211 <a989977@naver.com>
2023-12-22 09:43:14 +0900

    두수의 곱

---
+++ src/Codecata/Codecata.java
@@ -0,0 +1,15 @@
+package Codecata;
+
+public class Codecata {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(3,4));
+
+    }
+}
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
