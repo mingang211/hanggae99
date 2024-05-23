9cea91c80317122d37034a8d884a6f6c2a00e37c
mingang211 <a989977@naver.com>
2024-05-23 09:32:21 +0900

    n^2 배열 자르기

--- src/codecata8/Quest87.java
+++ src/codecata8/Quest87.java
@@ -18,7 +18,7 @@ class Solution87 {
     public List<Long> solution(int n, long left, long right) {
         List<Long> list = new ArrayList<>();
         for(long i = left; i< right+1; i++){
-            list.add(Math.max(i/n,i%n));
+            list.add(Math.max(i/n,i%n)+1);
         }
         return list;
     }
